5bc88c96eabe57425ffd8e1be61e84a68284494c
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-07 00:13:55 +0530

    Learnt about how to work with modules and pip in python

--- Day_3/main.py
+++ Day_3/main.py
@@ -1,1 +1,34 @@
 # This is main.py
+
+# Modules and pip in python
+
+import json
+
+
+students = '''
+{
+    "name": "Arnab",
+    "age" : 19,
+    "programming languages" : ["Java","Python","Javascript"],
+    "has friends": true
+}
+'''
+print(students)
+
+
+parsed_str = json.loads(students)
+
+print(parsed_str)
+
+print(type(parsed_str))
+
+data = {
+    "name": "Arnab",
+    "age" : 19,
+    "programming languages" : ["Java","Python","Javascript"],
+    "has friends": True
+}
+print(data)
+
+parsed_data = json.dumps(data)
+print(parsed_data)
