4b619fce031389350ed09099e6305b1524daf388
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-10 22:35:55 +0530

    Writing our first python program to kickstart 100 days of code journey

--- Day_4/main.py
+++ Day_4/main.py
@@ -1,1 +1,11 @@
 # This is main.py
+
+print("Hello World","Hi This is the first python program")
+print(5)
+print("Bye")
+
+# simple arithmetic operations can be done inside print function
+print(17*13)
+
+print(17/13)
+print(17//13)
