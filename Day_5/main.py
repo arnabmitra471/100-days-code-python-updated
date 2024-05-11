ef200fd0b06b32607b5b9499576e260b2dda2fd8
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-11 09:04:06 +0530

    Learnt about comments escape sequences and print function in detail

--- Day_5/main.py
+++ Day_5/main.py
@@ -1,1 +1,26 @@
 # This is main.py
+"""
+Please don't remove this line
+Author: Arnab
+Course: 100 days of code
+"""
+print('Hey I am a \'good boy\' \n and the viewer is also a good boy/girl')
+
+print("Hello World")  # printing hello world
+
+print("Sr No \t Name \t age")
+
+print("Good Morring everyone","I need to divide my time slots","Hey this is another string",sep="~",end="* ")
+print("This is yet another print statement")
+
+print("An apple a day keeps the doctor away",end="* ")
+print("Understood folks ?")
+
+print("Hey",66,32,sep="~",end="009\n")
+print("Arnab")
+
+# writing to a file using print function in python
+f = open("Arnab.txt","a")
+print("This is being written to Arnab.txt",file=f)
+f.close()
+
