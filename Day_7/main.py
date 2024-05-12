eb6e2e2260c15f1e3ad1bd65eb2cc1d56f715dbe
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-12 08:47:54 +0530

    Created a calculator using python

--- Day_7/main.py
+++ Day_7/main.py
@@ -1,1 +1,29 @@
 # This is main.py
+
+# Operators
+
+"""
+Arithmetic operators
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Floor division
+6. Exponentiation
+"""
+"""
+print(15+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(15%6)
+print(15**15)
+"""
+num1 = 34
+num2 = 12
+print("num1 + num2 = ",num1 + num2)
+print("num1 - num2 = ",num1 - num2)
+print("num1 * num2 = ",num1 * num2)
+print("num1 / num2 = ",num1 / num2)
+print("num1 // num2 = ",num1 // num2)
