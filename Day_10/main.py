69682eba45f34cdbb5929b1a5d95f5dfb4fcabc1
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-12 09:59:27 +0530

    Learnt about user input in python

--- Day_10/main.py
+++ Day_10/main.py
@@ -1,1 +1,9 @@
 # This is main.py
+
+# a = input("Enter your name: ")
+# print("My name is",a)
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+res = x + y
+print("The sum of x and y is",res)
