d4b23a32f082750714c4737e93a4e1f94a97ebd4
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-12 09:27:58 +0530

    Learnt about type casting in python

--- Day_9/main.py
+++ Day_9/main.py
@@ -1,1 +1,22 @@
 # This is main.py
+
+# Explicit type casting - Done by the programmer himself for converting one data type to another
+num1 = int("27")
+num2 = int("22")
+print("The sum of the numbers is",num1 + num2)
+
+# Two types of typecasting
+"""
+1. Explicit typecasting
+2. Implicit typecasting
+"""
+
+#implicit type casting - Python converts a lower order data type into higher order data type to prevent data loss
+
+num3 = 87.12
+num4 = 25
+res = num3 + num4
+print("The type of num3 is",type(num3))
+print("The type of num4 is",type(num4))
+print("The sum of num3 and num4 is",res)
+print("The type of res is",type(res))
