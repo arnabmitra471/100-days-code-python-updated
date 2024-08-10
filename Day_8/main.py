bea2f08acc9b3614c61b98cf30df88e906cca7b4
Arnab Mitra <arnabmitra471@gmail.com>
2024-08-10 08:23:59 +0530

    Learning more about print statement

--- Day_8/main.py
+++ Day_8/main.py
@@ -1,1 +1,7 @@
 # This is main.py
+print("Hello World",end=" ")
+print("I am Arnab and I am learning python",end=" ")
+print("This is another statement")
+
+print("Python is very interesting and awesome","I love python a lot","Hey there !!",sep="~",end="009 ")
+print("My friends are Vrinda and Shaarav")
