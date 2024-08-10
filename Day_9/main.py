67fe557a933333d34ff02db14e0d68a7bccbb049
Arnab Mitra <arnabmitra471@gmail.com>
2024-08-10 18:54:08 +0530

    Revised about type casting in python

--- Day_9/main.py
+++ Day_9/main.py
@@ -1,28 +1,56 @@
-# This is main.py
-
-# Explicit type casting - Done by the programmer himself for converting one data type to another
-num1 = int("27")
-num2 = int("22")
-print("The sum of the numbers is",num1 + num2)
-
 # Two types of typecasting
+
 """
-1. Explicit typecasting
-2. Implicit typecasting
+Implicit type casting
+Explicit type casting
 """
 
-#implicit type casting - Python converts a lower order data type into higher order data type to prevent data loss
-
-num3 = 87.12
-num4 = 25
-res = num3 + num4
-print("The type of num3 is",type(num3))
-print("The type of num4 is",type(num4))
-print("The sum of num3 and num4 is",res)
+num1 = 34 # implicit type casting 34 -> 34.0 + 12.6 = 46.6
+num2 = 12.6
+res = num1 + num2
+print("The sum is",res)
 print("The type of res is",type(res))
 
-n5_bin = bin(num2)
-print(n5_bin)
-print("The type of n5_bin is",type(n5_bin))
-hex_n5 = hex(num2)
-print("The hexadecimal representation of",n5_bin,"is",hex_n5)
+
+a = "61"
+b = "56"
+print("The sum of",int(a),"and",int(b),"is",int(a) + int(b))
+
+num3 = "90"
+num4 = "18761543"
+print(num3,type(num3))
+print(num4,type(num4))
+
+int_num3 = int(num3)
+int_num4 = int(num4)
+
+print(int_num3,type(int_num3))
+print(int_num4,type(int_num4))
+
+num3_bin = bin(int_num3)
+num3_oct = oct(int_num3)
+num4_oct = oct(int_num4)
+print(num3_bin)
+print(num3_oct)
+print(num4_oct)
+
+num5 = 671879871
+oct_num5 = oct(num5)
+print("The type of num5 is",type(num5))
+print(oct_num5,type(oct_num5))
+
+# num6 = "7590"
+# print(bin(num6)) This thows an error since num6 is a string and it cannot be interpreted as an integer
+
+num3_int = int(num3_bin,2)
+print(num3_int,type(num3_int))
+
+num5_hex = hex(num5)
+
+print(num5,"in hexadecimal is",num5_hex)
+num4_hex = hex(int_num4)
+print(int_num4,"in hexadecimal is",num4_hex)
+
+name = "Arnab"
+# print(int(name))
+# print(hex(name)) This throws an error as str cannot be interpreted as an integer
