164a9727c8631b4fae91879aae0f6a9fed16317e
Arnab Mitra <arnabmitra471@gmail.com>
2024-08-18 20:36:25 +0530

    Learnt about the all new match case statement in python

--- Day_15/main.py
+++ Day_15/main.py
@@ -1,1 +1,21 @@
 # This is main.py
+
+x = int(input("Enter a value between 0 to 4: "))
+
+match x:
+    case 0:
+        print("Case is 0")
+    case 1:
+        print("Case is 1")
+    case 2:
+        print("Case is 2")
+    case 3:
+        print("Case is 3")
+    case 4:
+        print("Case 4")
+    case _ if x != 90:
+        print(x,"is not 90")
+    case _ if x != 80:
+        print(x,"is not 80")
+    case _:
+        print("Invalid value entered. Please enter a valid value")
