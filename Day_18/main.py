df7c0daa9ac312c4e5f0aaca3bb63ef64720f766
Arnab Mitra <arnabmitra471@gmail.com>
2024-08-23 21:57:34 +0530

    Learnt about while loop in python

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,1 +1,21 @@
 # This is main.py
+
+# While loops in python
+
+count = 10
+
+while count >= 1:
+    print(count)
+    count -= 1
+else:
+    print("I am inside else")
+
+
+i = int(input("Enter a number: "))
+print(i)
+
+while i <= 100:
+    i = int(input("Enter a number again: "))
+    print(i)
+
+print("We are outside the while loop now")
