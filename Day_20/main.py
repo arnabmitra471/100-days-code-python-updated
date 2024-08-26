25942d9429e42fbe9a7ed2e81992f2287d0c5691
Arnab Mitra <arnabmitra471@gmail.com>
2024-08-26 17:12:03 +0530

    Learnt about functions and function arguments

--- Day_20/main.py
+++ Day_20/main.py
@@ -1,1 +1,36 @@
 # This is main.py
+
+def calculate_gmean(n1,n2,n3,n4):
+    gmean = (n1 * n2 * n3 * n4)**1/4
+    print("The geometric mean of the numbers is",gmean)
+
+def is_greater(a,b):
+    if a > b:
+        print("First number is greater")
+    elif a == b:
+       print("Both the numbers are equal")
+    else:
+        print("Second number is greater")
+
+def is_lesser(n1,n2):
+    if n1 < n2:
+        print(n1,"is lesser")
+    elif n1 == n2:
+        print("Both the numbers are equal")
+    else:
+        print(n2,"is lesser")
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+
+calculate_gmean(num1,num2,num3,num4)
+calculate_gmean(43.43,87.98,21.43,54.23)
+calculate_gmean(43.98,89.90,56.76,123.876)
+
+is_greater(num1,num2)
+is_greater(num3,num4)
+is_lesser(num1,num2)
+is_lesser(num3,num4)
--- Day_21/main.py
+++ Day_21/main.py
@@ -1,1 +1,61 @@
 # This is main.py
+
+# n is a required argument to be passed in the factorial function
+def factorial(n):
+    fact = 1
+    if n < 0:
+        print("Factorial is not defined for negative numbers")
+    elif n == 0:
+        return 1
+    else:
+        for i in range(1,n+1):
+            fact *= i
+        return fact
+
+res1 = factorial(5)
+res2 = factorial(10)
+res3 = factorial(12)
+res4 = factorial(13)
+
+num = int(input("Enter a number to obtain its factorial: "))
+res = factorial(num)
+print("The factorial of {0} is {1}".format(num,res))
+print(res1)
+print(res2)
+print(res3)
+print(res4)
+
+def average(n1,n3,n2=45):
+    print("The average is",(n1 + n2 + n3)/2)
+
+average(17,25)
+average(34,98)
+average(78,100)
+
+# Default arguments passed while creating the function
+average(43,23,198)
+
+# Keyword Arguments - Python will identify the arguments passed by the parameter name
+average(n3=67,n1=54)
+average(n1=23,n2=98,n3=76)
+
+# Variable length arguments
+
+def calculate_mean(*args):
+    total = 0
+    n = len(args)
+
+    for i in args:
+        total = total + i
+    mean = total/n
+    return mean
+
+nums = (34,86,98,12,65,45,18,20)
+res5 = calculate_mean(*nums)
+print(res5)
+
+def print_name(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+
+print_name(fname="Arnab",lname="Mitra")
